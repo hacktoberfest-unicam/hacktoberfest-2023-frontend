e3d8f4985483411678ca5aaab814f559fb978743
lollobeach <verduccii46@gmail.com>
2023-10-20 19:21:12 +0200

    test new changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,9 +13,13 @@ import { Link, Outlet } from "react-router-dom";
 import LogoHorizontal from "../images/logoHorizontal/hf10_horz_fcl_rgb.png";
 import Icon from "../images/logomark_icon/hf10_icon_fcd_cmyk.png";
 
+import ScreenSmall from "../images/background/code-coding.gif"
+
 import BackgroundLaptop from "../images/background/bigScreen.jpg";
 import { useState } from "react";
 import { theme } from "../theme/customTheme";
+
+import TypewriterEffect from "react-typewriter-effect";
 
 export default function Layout() {
   const [showDynamicIsland, setShowDynamicIsland] = useState(false);
@@ -45,13 +49,31 @@ export default function Layout() {
 
   for (let i = 0; i <= 100; i += 1) {
     animation[`${i}%`] = {
-      background: `linear-gradient(${i * 3.6}deg, rgba(210,184,99,1) 17%, rgba(255,251,164,1) 44%, rgba(195,188,195,1) 67%);`,
+      background: `linear-gradient(${
+        i * 3.6
+      }deg, rgba(210,184,99,1) 17%, rgba(255,251,164,1) 44%, rgba(195,188,195,1) 67%);`,
     };
   }
 
   return (
     <div>
-      {/* <Box component="img" src={BackgroundLaptop} position="fixed" /> */}
+      <Box bgcolor="#040204" height="100vh" sx={{ display: { md: "none", xs: "flex" }, flexDirection: "column", alignItems: "center" }}>
+        <Box bgcolor="#040204" height={26} display="flex" justifyContent="center" padding={2}>
+          <TypewriterEffect
+            textStyle={{
+              fontWeight: 800,
+              fontSize: 20,
+              color: "#ad832d",
+              textAlign: "center"
+            }}
+            startDelay={100}
+            text="Sorry, the screen is too small, use a laptop"
+            typeSpeed={50}
+            hideCursorAfterText={true}
+          />
+        </Box>
+          <img src={ScreenSmall} alt="screenToSmall" width={500} />
+      </Box>
       <AppBar position="absolute" color="transparent" sx={{ boxShadow: 0 }}>
         <Toolbar
           disableGutters
@@ -60,7 +82,7 @@ export default function Layout() {
             maxWidth: 1300,
             marginX: "auto",
             marginY: 0,
-            gap: 30,
+            gap: 20,
           }}
         >
           <Link to="/">
@@ -83,7 +105,8 @@ export default function Layout() {
             <Button
               variant="outlined"
               sx={{ marginLeft: "auto" }}
-              href="profile"
+              // href="profile"
+              href={`https://github.com/login/oauth/authorize?scope=user&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URL}`}
             >
               <Typography variant="h5" color="secondary.main">
                 Profile
@@ -92,7 +115,15 @@ export default function Layout() {
           </Box>
         </Toolbar>
       </AppBar>
-      <Box width="100%" position="absolute" minHeight="100vh" sx={{ backgroundImage: `url(${BackgroundLaptop})` }}>
+      <Box
+        width="100%"
+        position="absolute"
+        minHeight="100vh"
+        sx={{
+          backgroundImage: `url(${BackgroundLaptop})`,
+          display: { md: "inline", xs: "none" },
+        }}
+      >
         <Box
           maxWidth={1300}
           marginX="auto"
@@ -117,16 +148,16 @@ export default function Layout() {
                   animationIterationCount: "infinite",
                   "@keyframes borderGradient": animation,
                 }}
-                borderRadius={10}
+                borderRadius={8}
                 padding={0.5}
               >
                 <Box
                   bgcolor="#000000"
-                  borderRadius={10}
+                  borderRadius={8}
                   display="flex"
                   justifyContent="space-around"
                   paddingX={2}
-                  paddingY={1}
+                  paddingY={3}
                 >
                   <Box component="a" href="/" height={61} padding={0.2}>
                     <img src={Icon} alt="hacktoberfest" />
@@ -137,7 +168,7 @@ export default function Layout() {
                         <Button
                           key={page.label}
                           href={page.link}
-                          sx={{ borderRadius: 10, padding: 2 }}
+                          sx={{ borderRadius: 8, padding: 2 }}
                         >
                           <Typography variant="h5" color="primary.light">
                             {page.label}
@@ -184,10 +215,68 @@ export default function Layout() {
             </Typography>
             <Stack>
               <Typography variant="body1" color="primary.dark">
-                Backend: <a href="https://github.com/HarlockOfficial" target="_blank" rel="noreferrer" style={{ textDecoration: "none", color: theme.palette.primary.dark }}>Francesco Moschella</a>, <a href="https://github.com/ArghgrA" target="_blank" rel="noreferrer" style={{ textDecoration: "none", color: theme.palette.primary.dark }}>Vincenzo Petrillo</a>
+                Backend:{" "}
+                <a
+                  href="https://github.com/HarlockOfficial"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{
+                    textDecoration: "none",
+                    color: theme.palette.primary.dark,
+                  }}
+                >
+                  Francesco Moschella
+                </a>
+                ,{" "}
+                <a
+                  href="https://github.com/ArghgrA"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{
+                    textDecoration: "none",
+                    color: theme.palette.primary.dark,
+                  }}
+                >
+                  Vincenzo Petrillo
+                </a>
               </Typography>
               <Typography variant="body1" color="primary.dark">
-                Frontend: <a href="https://github.com/lollobeach" target="_blank" rel="noreferrer" style={{ textDecoration: "none", color: theme.palette.primary.dark }}>Lorenzo Verducci</a>, <a href="https://github.com/giorgiosld" target="_blank" rel="noreferrer" style={{ textDecoration: "none", color: theme.palette.primary.dark }}>Giorgio Saldana</a>, <a href="https://github.com/Frascott05" target="_blank" rel="noreferrer" style={{ textDecoration: "none", color: theme.palette.primary.dark }}>Francesco Scotti</a>
+                Frontend:{" "}
+                <a
+                  href="https://github.com/lollobeach"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{
+                    textDecoration: "none",
+                    color: theme.palette.primary.dark,
+                  }}
+                >
+                  Lorenzo Verducci
+                </a>
+                ,{" "}
+                <a
+                  href="https://github.com/giorgiosld"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{
+                    textDecoration: "none",
+                    color: theme.palette.primary.dark,
+                  }}
+                >
+                  Giorgio Saldana
+                </a>
+                ,{" "}
+                <a
+                  href="https://github.com/Frascott05"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{
+                    textDecoration: "none",
+                    color: theme.palette.primary.dark,
+                  }}
+                >
+                  Francesco Scotti
+                </a>
               </Typography>
             </Stack>
           </Box>
