6d40d8e63cfeb5836641e9f78ceb79a1c15b5b29
Harlock Official <harlockofficial.github@gmail.com>
2023-10-21 01:03:37 +0200

    [edit] fix login process

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,35 +1,31 @@
-import  React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import AdminPage from '../pages/AdminPage'
 import UserInfo from '../components/UserPage/UserInfo';
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 export default function UserPage() {
 
-  const [isAdmin, setIsAdmin] = useState(true);
-  const [users, setUsers] = useState([])
+    const [isAdmin, setIsAdmin] = useState(true);
+    const [users, setUsers] = useState([])
 
-  // Funzione per impostare isAdmin su true se l'utente è un amministratore
-  const handlePower = useEffect(() => {
-    const apiUsers = 'http://127.0.0.1:8000/api/user/all' 
-    axios.get(apiUsers).then((response) => {
-      setUsers(response.data)
+    // Funzione per impostare isAdmin su true se l'utente è un amministratore
+    const handlePower = useEffect(() => {
+        const apiUsers = `${process.env.REACT_APP_BACKEND_URL}api/user/all`
+        axios.get(apiUsers).then((response) => {
+            setUsers(response.data)
 
-  })
-  .catch((error) => {
-      console.error("Zio pera")
-  });
-  }, []);
+        }).catch((error) => {
+                console.error("Zio pera")
+        });
+        console.log(Cookies.get('authorization'))
+    }, []);
 
 
-  return (
-    <div>
-      {isAdmin ?
-        <AdminPage user={"harlockOfficial"}/>
-      :
-        <UserInfo user={users[1]} />           
-      }
-    </div>
-    
-    
-  )
+    return (<div>
+            {isAdmin ? <AdminPage user={"harlockOfficial"}/> : <UserInfo user={users[1]}/>}
+        </div>
+
+
+    )
 }
